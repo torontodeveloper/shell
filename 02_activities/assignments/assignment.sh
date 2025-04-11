f04ebb49519f8a0592b82900432d4c460e4c271a
nageswarkv@gmail.com <nageswarkv@gmail.com>
2025-04-11 14:33:10 -0400

    fixing files output issue

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -45,7 +45,7 @@ rm ./data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch ./data/inventory.txt
 cd ./data
-ls -r ./processed > ./inventory.txt
+ls -r ./processed/* > ./inventory.txt
 cd ../
 
 ###########################################
